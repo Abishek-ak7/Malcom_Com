dcca2deecb110ece8dddfb99cf84c94a61c52b57
Abishek-7ak <abishekcsecs@gmail.com>
2024-05-20 11:20:03 +0530

    Malcom content

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,13 +1,12 @@
 import Navigation from './navigation';
 import '../assets/Home.css'
+import img from '../assets/images/Malcom1.png'
 
 function home() {
   return (
     <div className="Home">
       <Navigation className="m-11"/>
-      
-      <img src='../assets/images/Malcom1.png' alt="AK"></img>
-      
+      <img src={img} className='mx-auto mt-40' alt="AK"></img>
     </div>
   );
 }
