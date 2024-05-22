357683a28003929ccd7d49992ed82dfb83d3cc62
janetshinyv <janetshinyv.cse2022@citchennai.net>
2024-05-22 20:45:18 +0530

    keyboard image added but styling not completed

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,36 +1,44 @@
 import React from "react";
-import '../assets/Navigation.css'
-import logo from '../assets/images/logo.png'
+import '../assets/Navigation.css';
+import logo from '../assets/images/logo.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faX } from '@fortawesome/free-solid-svg-icons';
+
+
+
 
 function Navigation() {
   return (
   <div>
-    <div className="flex  p-3">
-
-        <h1 className="ak  text-white font-extrabold font-serif">MALCOM COMPANY</h1>
-        <div className="flex mx-auto space-x-24  font-roboto text-sm">
-          <a href="" className="hover:text-purple-300 text-purple-400 text-base hover:underline hover:underline-offset-4 decoration-white decoration-solid lg:block md:block sm:hidden ">Product</a>
-          <a href="" className=" hover:text-purple-300 text-purple-400 text-base hover:underline hover:underline-offset-4 decoration-white decoration-solid b-2  lg:block md:block sm:hidden">Services</a>
-          <a href="" className=" hover:text-purple-300 text-purple-400 text-base hover:underline hover:underline-offset-4 decoration-white decoration-solid b-2 lg:block md:block sm:hidden">Careers</a>
-          <a href="" className="hover:text-purple-300 text-purple-400 text-base hover:underline hover:underline-offset-4 decoration-white decoration-solid b-2  lg:block md:block sm:hidden">About</a>
-
+      <nav className="flex justify-between items-center w-[92%]">
+        <div className="flex w-26  p-3">
+          <h1 className="ak  text-white font-extrabold font-serif ">MALCOM COMPANY</h1>
         </div>
-        <div className="flex space-x-6">
-          <button className="hover:bg-white text-lg font-bold hover:w-28 w-28 font-Mr mr-10 hover:h-7 hover:rounded-2xl lg:block md:block sm:hidden hover:text-purple-800 hover:font-bold text-white text-center">Donate Us</button>    
-          <button class="md:hidden lg:hidden sm:block bg-white   rounded-sm"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-           <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-           </svg></button>   
-            </div>
-     </div>
-     <div className="lg:hidden md:hidden sm:block text-md border w-1/4 ml-auto ">
-          <a href="" className=" text-purple-400 p-2 flex hover:text-white">Product</a>
-          <a href="" className="text-purple-400 p-2 flex hover:text-white"> Services</a>
-          <a href="" className="text-purple-400 p-2 flex hover:text-white "> Careers</a>
-          <a href="" className="text-purple-400 p-2 flex  hover:text-white"> About</a>
-          <button className="hover:bg-white text-md font-sans p-1 ml-1  hover:rounded-xl lg:hidden md:hidden sm:block hover:text-purple-800 hover:font-bold text-purple-400 ">Donate Us</button>        </div>
 
-     </div>
+        <div className="md:static absolute md:bg-black lg:bg-transparent md:min-h-fit min-h-[60vh] left-0 top-[-100%] md:w-auto w-full flex items-center px-5">
+          <ul className="flex md:flex-row flex-col md:items-center md:gap-[6vw] gap-6">
+            <li><a href="" className="hover:text-purple-300 text-purple-400 text-base hover:underline hover:underline-offset-4 decoration-white decoration-solid ">Product</a></li>
+            <li><a href="" className="hover:text-purple-300 text-purple-400 text-base hover:underline hover:underline-offset-4 decoration-white decoration-solid ">Services</a></li>
+            <li><a href="" className="hover:text-purple-300 text-purple-400 text-base hover:underline hover:underline-offset-4 decoration-white decoration-solid ">Careers</a></li>
+            <li><a href="" className="hover:text-purple-300 text-purple-400 text-base hover:underline hover:underline-offset-4 decoration-white decoration-solid ">About</a></li>
+          </ul>
+        </div>
+        <div>
+          <button className=" hover:bg-white text-lg font-bold hover:w-28 w-28 font-Mr  hover:h-7 hover:rounded-2xl hover:text-purple-800 hover:font-bold text-white">Donate Us</button>
+        </div>
+
+        <div className=" items-center cursor-pointer md:hidden lg:hidden sm:block ">
+        <FontAwesomeIcon icon={faBars} style={{ color: 'white', width:'25px', height: '35px' }} size="6x" />
+        <FontAwesomeIcon icon={faX} style={{ color: 'white', width:'25px', height: '35px' }} />
+        </div>
+      </nav>
+    </div>
   );
 }
 
 export default Navigation;
+
+
+
+
